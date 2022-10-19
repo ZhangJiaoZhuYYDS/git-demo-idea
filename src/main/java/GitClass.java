a7de378a893c658258e9d28e4ed182e1b407b41c
ZhangGaoFeng <1274929812@qq.com>
2022-10-19 15:11:28 +0800

    master commit2

--- src/main/java/GitClass.java
+++ src/main/java/GitClass.java
@@ -2,6 +2,6 @@ public class GitClass {
     public static void main(String[] args) {
         System.out.println(22222);
         System.out.println(44444);
-        System.out.println(55555);
+        System.out.println("master");
     }
 }
