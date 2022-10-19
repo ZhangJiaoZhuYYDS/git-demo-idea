a9f9479557b65a9fed5dd5196c2736fada1a29d9
ZhangGaoFeng <1274929812@qq.com>
2022-10-19 15:10:45 +0800

    hot-fix commit2

--- src/main/java/GitClass.java
+++ src/main/java/GitClass.java
@@ -2,6 +2,6 @@ public class GitClass {
     public static void main(String[] args) {
         System.out.println(22222);
         System.out.println(66666);
-        System.out.println(11111);
+        System.out.println("hot-fix");
     }
 }
